6cd284511115159c1c41c9b93e2b032ce4f5f27e
maleks93 <sangram.s.malekar@gmail.com>
2019-05-03 17:14:13 -0700

    Completed the module

--- jobs/app.py
+++ jobs/app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 def open_connection():
     connection = getattr(g, '_connection', None)
     if connection == None:
-        connect = g._connection = sqlite3.connect(PATH)
-    connection.row_factory = sqlite3.row_factory
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
     return connection
 
 def execute_sql(sql, values=(), commit=False, single=False):
